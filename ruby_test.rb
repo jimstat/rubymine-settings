3c9233f3e530190b4247a3637436da526312e507
Jim Wheaton <jim@getstat.com>
2017-07-31 10:55:10 -0700

    another test

--- ruby_test.rb
+++ ruby_test.rb
@@ -19,7 +19,23 @@ end
 
 
 
-def test_indents
+def test_indents2
+  ok1 = 'ok 1'
+  not_okd = 'too much indent'
+  'this will '\
+      'continute'
+
+  my_hash = { boo: 'do' }
+
+
+  this_is_a_dif = {dif:"ooo"}
+
+  second_dif ={ddd:"fail me"}
+
+  "foo"
+end
+
+def test_indents3
   ok1 = 'ok 1'
   not_okd = 'too much indent'
   'this will '\
