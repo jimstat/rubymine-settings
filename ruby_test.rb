bc9c973171f3997bf9987c2d8e8af38c52ff5b8c
Jim Wheaton <jim@getstat.com>
2017-07-31 10:49:33 -0700

    Test

--- ruby_test.rb
+++ ruby_test.rb
@@ -16,3 +16,21 @@ end
 def j
   "not very useful name"
 end
+
+
+
+def test_indents
+  ok1 = 'ok 1'
+  not_okd = 'too much indent'
+  'this will '\
+      'continute'
+
+  my_hash = { boo: 'do' }
+
+
+  this_is_a_dif = {dif:"ooo"}
+
+  second_dif ={ddd:"fail me"}
+
+  "foo"
+end
